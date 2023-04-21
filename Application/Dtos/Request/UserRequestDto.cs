1e9424ce068ad1ff729f8131f8d599dbf48c7a6a
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-04-22 00:01:59 +0100

    create user request dto with data notation

---
+++ Application/Dtos/Request/UserRequestDto.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace QuestionaryApp.Application.Dtos.Request
+{
+    public class UserRequestDto
+    {
+        [Required(ErrorMessage = "Field name is required")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "This field must contain between 3 and 40 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Field email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Field email is required")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "This field must contain between 3 and 20 characters")]
+        public string CodeName { get; set; }
+
+        [Required(ErrorMessage = "Field email is required")]
+        public string Password { get; set; }
+    }
+}
