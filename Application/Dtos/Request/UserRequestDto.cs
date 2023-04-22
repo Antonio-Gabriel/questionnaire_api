55b1eea9b0da60103caa35914c20d24f947409b6
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-04-22 01:09:25 +0100

    change fieldname on data notation

--- Application/Dtos/Request/UserRequestDto.cs
+++ Application/Dtos/Request/UserRequestDto.cs
@@ -11,11 +11,11 @@ namespace QuestionaryApp.Application.Dtos.Request
         [Required(ErrorMessage = "Field email is required")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Field email is required")]
+        [Required(ErrorMessage = "Field codeName is required")]
         [StringLength(40, MinimumLength = 3, ErrorMessage = "This field must contain between 3 and 20 characters")]
         public string CodeName { get; set; }
 
-        [Required(ErrorMessage = "Field email is required")]
+        [Required(ErrorMessage = "Field password is required")]
         public string Password { get; set; }
     }
 }
