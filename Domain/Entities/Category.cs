01a3294e0a337d0818ac9c3c9ffce5175e42fc2c
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2023-04-22 03:13:39 +0100

    change relation between objects in category

--- Domain/Entities/Category.cs
+++ Domain/Entities/Category.cs
@@ -2,7 +2,7 @@ namespace QuestionaryApp.Domain.Entities
 {
     public class Category : BaseAuditableEntity
     {
-        public string Name { get; set; }      
-        public Questionnaire Questionnaire { get; set; }
+        public string Name { get; set; }
+        public ICollection<Questionnaire> Questionnaire { get; set; }
     }
 }
